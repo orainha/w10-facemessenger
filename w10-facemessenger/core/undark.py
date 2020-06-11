e73c8b2d8b6736b8febaec72442e35cadf1d9be7
orainha <darkboy880@gmail.com>
2020-06-11 21:15:13 +0100

    Implemented undark csv report

---
+++ w10-facemessenger/core/undark.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import subprocess
+import csv
+import json
+import bs4
+import requests
+import shutil
+
+import utils.files as utils
+import threading
+
+
+# XXX (ricardoapl) Fix this non-pythonic mess!
+# TEMPLATE_FILENAME = os.path.join(os.path.dirname(__file__), r'..\templates\template_images.html')
+# REPORT_FILENAME = 'report_images.html'
+NEW_FILE_PATH = ''
+PATH = ''
+
+
+def report_csv(delim):
+    """
+    Search for deleted data from src directory into dst file by running undark.exe.
+    """
+    input_path = PATH
+    dst_path = NEW_FILE_PATH 
+    filename = '\\report-undark.csv'
+    dst_path = dst_path + filename
+    db_path = utils.get_db_path(input_path)
+    undark = os.path.join(os.path.dirname(__file__), '..\\undark.exe')
+    with open(dst_path,'w') as f:
+        args = [
+            undark,
+            '-i', db_path,
+            '--freespace',
+            # '--fine-search',
+            # '--removed-only',
+        ]
+        subprocess.Popen(args,stdout=f)
+
+def paths(args):
+    input_file_path(args.input)
+    output_file_path(args.output)
+
+
+def input_file_path(path):
+    # XXX (orainha) Procurar por utilizadores dando apenas o drive?
+    # XXX (orainha) Where is PATH used?
+    global PATH
+    PATH = utils.get_input_file_path(path)
+
+
+def output_file_path(path):
+    global NEW_FILE_PATH
+    NEW_FILE_PATH = utils.get_output_file_path(path)
+
+
+
